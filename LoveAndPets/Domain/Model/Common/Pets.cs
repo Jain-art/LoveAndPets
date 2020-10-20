8819b39fc4552b70f9033ef2336e289b163959e6
Женя <tsparou_e@mail.ru>
2020-10-20 15:32:16 +0400

    Начало оаботы над проектом

---
+++ LoveAndPets/Domain/Model/Common/Pets.cs
@@ -0,0 +1,28 @@
+﻿using LoveAndPets.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoveAndPets.Domain.Model.Common
+{
+    /// <summary>
+    /// Анкета питомцев
+    /// </summary>
+    public class Pets:Entity
+    {
+        /// <summary>
+        /// Индентификатор хозяина животного
+        /// </summary>
+        public long UserId { get; set; }
+        /// <summary>
+        /// Индентификатор типа животного
+        /// </summary>
+        public long AnimalId { get; set; }
+        /// <summary>
+        /// Индентификатор породы животного
+        /// </summary>
+        public long BreedId { get; set; }
+
+    }
+}
