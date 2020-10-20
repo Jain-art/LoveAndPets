cfb99c36f08392cfac655f91e6035209462d4a8c
Женя <tsparou_e@mail.ru>
2020-10-20 15:32:16 +0400

    Начало оаботы над проектом

---
+++ LoveAndPets/Domain/Model/Common/Entity.cs
@@ -0,0 +1,18 @@
+﻿namespace LoveAndPets.Model.Common
+{
+    /// <summary>
+    /// Интерфейс модели сущности предметной области
+    /// </summary>
+    public abstract class Entity
+    {
+        /// <summary>
+        /// создание экземпляра модели мущности предметной области
+        /// </summary>
+        protected Entity() { }
+
+        /// <summary>
+        /// Идентификатор сущности
+        /// </summary>
+        public virtual long Id { get; set; }
+    }
+}
---
+++ LoveAndPets/Domain/Model/Common/Services.cs
@@ -0,0 +1,13 @@
+﻿using LoveAndPets.Model.Common;
+
+namespace LoveAndPets.Domain.Model.Common
+{
+    public class Services : Entity
+    {
+        /// <summary>
+        /// название услуг, оказываемые врачами
+        /// </summary>
+        public string ServiceName { get; set; }
+
+    }
+}
